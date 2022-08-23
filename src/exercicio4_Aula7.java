513df71c1cec7cbca9b67839dbbf0ba382979e6a
blandamellus <blandamellus@gmail.com>
2022-08-23 20:52:54 -0300

    exercicios e arquivo pra exercicio

---
+++ src/exercicio4_Aula7.java
@@ -0,0 +1,12 @@
+public class exercicio4_Aula7 {
+    public static void x_main(String[] args) throws Exception{
+        
+    for(int i = 0; i <= 10; i++){
+        System.out.println("Mensagem a ser repetida " + i);
+
+    }
+        
+    
+            
+    }
+}
---
+++ src/exercicioRepeticao_Aula7.java
@@ -0,0 +1,8 @@
+public class exercicioRepeticao_Aula7 {
+    public static void main(String[] args) throws Exception{
+      // espaço para exercicio
+
+
+      
+    }
+}
