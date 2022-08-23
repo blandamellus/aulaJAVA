74da8290cfc14feb77fafc660190c2850008c68b
blandamellus <blandamellus@gmail.com>
2022-08-23 19:41:41 -0300

    Primeiros arquivos

---
+++ src/exercicioAula7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class exercicioAula7 {
+    public static void main(String[] args) throws Exception{
+        // Faça um Programa que leia três números e mostre o maior deles.
+        int numero1, numero2, numero3; 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe um número: ");
+        numero1 = entrada.nextInt();
+        
+        System.out.println("Informe um número00: ");
+        numero2 = entrada.nextInt();
+        
+        System.out.println("Informe um número: ");
+        numero3 = entrada.nextInt();
+        
+        if(numero1 > numero2 && numero1 > numero3){
+            System.out.println("Primeiro número é maio:r " + numero1);
+        } else if(numero2 > numero1 && numero2 > numero3){
+            System.out.println("segundo número é maior: " + numero2);
+        } else{
+            System.out.println("Terceiro número é maior: " + numero3);
+        }
+    }
+}
+
+
+
