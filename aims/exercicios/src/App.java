baed1cd6a2a0b1917a856d7183812f3ef46d1da9
aime <aime.g.ganzer@gmail.com>
2022-08-25 21:00:17 -0300

    teste

--- aims/exercicios/src/App.java
+++ aims/exercicios/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("seu primeiro oi mundo, aims!");
-        
+
+        // teste
+
     }
 }
