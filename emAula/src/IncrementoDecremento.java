65ffd288511fa11b880eeecb83f3209ef804c396
blandamellus <blandamellus@gmail.com>
2022-08-27 12:09:07 -0300

    aula 9 (27/08 sábado)

---
+++ emAula/src/IncrementoDecremento.java
@@ -0,0 +1,19 @@
+public class IncrementoDecremento {
+    public static void main(String[] args) throws Exception {
+        
+        int numero = 3;
+
+        numero = numero + 1;
+        System.out.println(numero);
+        
+        
+        System.out.println("incremento: "+ ++numero);
+        System.out.println("decremento: "+ --numero);
+
+        
+
+
+        
+  
+    }
+}
