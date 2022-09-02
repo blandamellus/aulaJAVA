5b6e253a95a86bf1d1f5af360a3a107d2cfc0498
othavio <20000403@liberato.com.br>
2022-09-01 21:02:02 -0300

    exercicios

---
+++ othavio/exercicios/src/VolumeLatas.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VolumeLatas{
+    public static void main(String[] args) throws Exception{
+
+        Scanner entrada = new Scanner(System.in);
+        double Volume, raio, altura;
+
+        System.out.print("Digite o raio da lata: ");
+        raio = entrada.nextDouble();
+
+        System.out.print("Digite a altura: ");
+        altura = entrada.nextDouble();
+
+        Volume = 3.14159 * raio * raio * altura;
+
+        System.out.print("O Volume da lata é " + Volume + "\n");
+
+    }
+}
