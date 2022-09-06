e0f22cb57bb471b87cb28f77794ae6113b410209
blandamellus <blandamellus@gmail.com>
2022-09-06 17:31:12 -0300

    exercícios aula 12

---
+++ blanda/src/ExplorandoString.java
@@ -0,0 +1,7 @@
+public class ExplorandoString {
+    public static void main(String[] args) throws Exception {
+        byte bytes [ ] = {65, 66, 67}; 
+        String s = new String (bytes);
+        System.out.println(s);
+    }
+}
