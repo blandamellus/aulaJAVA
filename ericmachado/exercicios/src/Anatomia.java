b1bedd88e45e8d17cb84eb7dc1123cb28f8c6c86
eric <ericmaachado77@gmail.com>
2022-09-06 20:58:31 -0300

    anatomia

---
+++ ericmachado/exercicios/src/Anatomia.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Anatomia {
+    public static void main(String[] args) throws Exception {
+    Scanner entrada = new Scanner(System.in);
+
+    // exercício 1 
+    System.out.println("O texto de entrada tem tamanho. " + textoInicial);
+
+    
+
+    // exercício 4
+
+    char letraInicial = textoInicial.charAt(index:0);
+    System.out.println(letraInicial);
+
+    char letra = Character.toUpperCase(letraInicial);
+    System.out.println(letra);
+
+    String letra = Character.toUpperCase(letraInicial);
+
+    if(textoInicial.charAt(index:0) == ){
+      System.out.println(x: "A letra está maiúscula");
+    } else {
+        System.out.println(x: "A letra está minúscula");
+    }
