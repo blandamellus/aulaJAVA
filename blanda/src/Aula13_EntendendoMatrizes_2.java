38fe68f26daeee06841ef241dd152e7c65419f48
blandamellus <blandamellus@gmail.com>
2022-09-08 20:45:54 -0300

    aula 13

---
+++ blanda/src/Aula13_EntendendoMatrizes_2.java
@@ -0,0 +1,13 @@
+// (8 , 1) (1 , 8) (1 , 11) (3 , 13) (6 , 13) (8 , 11) (10 , 13) (13 , 13) (15 , 11) (15 ,8) (8 , 1)
+
+import java.util.Scanner;
+
+public class Aula13_EntendendoMatrizes_2 {
+    public static void main(String[] args) throws Exception {
+
+        String matriz
+
+
+
+    }
+}
