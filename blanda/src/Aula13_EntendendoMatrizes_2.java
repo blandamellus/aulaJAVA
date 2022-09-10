ae5388e82e5988fb75cbe2f016642a5343694563
blandamellus <blandamellus@gmail.com>
2022-09-10 01:25:04 -0300

    teste

--- blanda/src/Aula13_EntendendoMatrizes_2.java
+++ blanda/src/Aula13_EntendendoMatrizes_2.java
@@ -1,13 +1,36 @@
-// Desenho 1: (8 , 1) (1 , 8) (1 , 11) (3 , 13) (6 , 13) (8 , 11) (10 , 13) (13 , 13) (15 , 11) (15 ,8) (8 , 1)
+
+// Desenho 1: 
+
+
 
 import java.util.Scanner;
 
 public class Aula13_EntendendoMatrizes_2 {
     public static void main(String[] args) throws Exception {
 
-        String matriz
+        String [][] matriz = new String[16][16];
+        matriz[8][1]    = " X ";
+        matriz[1][8]    = " X ";
+        matriz[1][11]   = " X ";
+        matriz[3][13]   = " X ";
+        matriz[6][13]   = " X ";
+        matriz[8][11]   = " X ";
+        matriz[10][13]  = " X ";
+        matriz[13][13]  = " X ";
+        matriz[15][11]  = " X ";
+        matriz[15][8]   = " X ";
+        matriz[8][1]    = " X ";
 
 
-
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz.length; j++){
+                if(matriz[i][j] == null){
+                    System.out.print("___");
+                }else{
+                    System.out.print(matriz[i][j]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
