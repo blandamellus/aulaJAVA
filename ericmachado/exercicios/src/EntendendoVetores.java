b72a69bd33a89fbf1c04f777a0d0fc0fe8050e8d
eric <ericmaachado77@gmail.com>
2022-09-22 20:17:02 -0300

    subindo a vida

---
+++ ericmachado/exercicios/src/EntendendoVetores.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class EntendendoVetores {
+    public static void maid(String[] args) throws Exception {
+
+        Scanner entrada = new Scanner (System.in);
+        Double [] vetorNotas = new Double [4];
+        
+
+    }
+}
