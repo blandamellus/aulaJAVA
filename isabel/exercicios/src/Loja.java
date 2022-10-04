87f7b104ccd334ccec83f2691cd5463f801296c6
Pablo <pablohomem0@gmail.com>
2022-10-04 18:36:42 -0300

    subindo

---
+++ isabel/exercicios/src/Loja.java
@@ -0,0 +1,19 @@
+class Lojaeletronicos {
+    String ssd;
+    String hd;
+    String Placamae;
+    String Placavideo;
+    String Memram;
+
+
+}
+
+public class Loja {
+    public static void main(String[] args) throws Exception {
+
+
+        
+    }
+}
+
+
