cb8e234c6125a5d169e5fc59d01f947deb9abd08
blandamellus <blandamellus@gmail.com>
2022-10-04 20:56:59 -0300

    aula 4 - loja eletronicos

--- blanda/src/Loja.java
+++ blanda/src/Loja.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class LojaEletronicos{
     String ssd;
     String hd;
@@ -7,15 +9,17 @@ class LojaEletronicos{
     String gabinete;
     String fonte;
     String mouse;
+    String[] pedido = new String[8];
 
-    public String[] vetorPedido(){
-        String [] vPedidos = new String[8];
-        return vPedidos;
+    public void fazerPedido(String item, int posicao){
+        
+        this.pedido[posicao] = item; 
+        //return this.pedido;
     }
-    //https://github.com/blandamellus/aulaJAVA/
+    
 
     public void vender(String nomeCliente){
-        System.out.println("Estou vendo um pedido fechado.");
+        System.out.println("Estou vendendo um pedido fechado para " + nomeCliente );
     }
     
     public void comprar(int cnpj, String nomeProduto){
@@ -23,14 +27,20 @@ class LojaEletronicos{
         System.out.println("Pedido de " +  nomeProduto + " realizado em nome do CNPJ: " + cnpj);
     } 
 
-    public void trocar(){}
-    public void consertar(){}
+    public void trocar(String pecaComProblemas, String nomeCliente){
+        System.out.println("Pedido de assistência para peça " + pecaComProblemas + " em nome do Cliente " + nomeCliente);
+    }
+
+    public void consertar(String dataAgendamento, String dataRetirada, String equipamento){
+        System.out.println("O equipamento " + equipamento + " foi recebido na data " + dataAgendamento + " e estará finalizado na data " + dataRetirada);
+    }
 
 }
 public class Loja {
     public static void main(String[] args) throws Exception {      
-        LojaEletronicos gamerX = new LojaEletronicos();
+        Scanner entrada = new Scanner(System.in);
 
+        LojaEletronicos gamerX = new LojaEletronicos();
         gamerX.fonte = "Fonte 600V";
         gamerX.gabinete = "gabinete gamer";
         gamerX.hd = "1Tb 7000rpm";
@@ -39,11 +49,36 @@ public class Loja {
         gamerX.placaMae = "Asus";
         gamerX.placaVideo = "RTX 4090TI";
         gamerX.ssd = "500 Gb pcle";
+        
+        // cnpj = 9 digitos
+        gamerX.comprar(105000151, "NVIDIA");
+        gamerX.vender("Antonio");
+        gamerX.trocar("mouse", "Maria");
+        
 
+        gamerX.fazerPedido(gamerX.gabinete, 0);
+        gamerX.fazerPedido(gamerX.hd, 1);
+        gamerX.fazerPedido(gamerX.mouse, 2);
 
+                
+        System.out.println("Exibir o carrinho 0 " + gamerX.pedido[0]);
+        System.out.println("Exibir o carrinho 1 " + gamerX.pedido[1]);
+        System.out.println("Exibir o carrinho 2 " + gamerX.pedido[2]);
 
+        // testando com entrada do usuário
+        String dataAgenda;
+        String dataRetira;
+        String equipamento;
+        System.out.println("O que você deseja consertar? ");
+        equipamento = entrada.next();
 
+        System.out.println("Quando você deseja consertar? ");
+        dataAgenda = entrada.next();
+        
+        System.out.println("Quando você deseja retirar? ");
+        dataRetira = entrada.next();
 
+        gamerX.consertar(dataAgenda, dataRetira, equipamento);
 
     }
 }
