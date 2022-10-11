1709345f2157ad94736e552885c4b77adedb7adf
blandamellus <blandamellus@gmail.com>
2022-10-11 20:56:28 -0300

    teste

--- blanda/src/CanetaTeste.java
+++ blanda/src/CanetaTeste.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CanetaTeste {
     public static void main(String[] args) throws Exception {
         
@@ -13,8 +15,43 @@ public class CanetaTeste {
         tilibra.setTamanho(0.5);
         System.out.println(tilibra.toString());
         
+        Caneta havan = new Caneta("Havan", "laranja", 0.7);
+        System.out.println(havan.toString());
 
+        Caneta faberCastel = new Caneta("Faber Castel");
+        System.out.println(faberCastel.toString());
+        
+        Scanner entrada = new Scanner(System.in);
 
+        String cor, marca;
+        Double tamanho;
+
+        System.out.println("Entre com a marca da caneta: ");
+        marca = entrada.next();
+
+        System.out.println("Entre com uma cor de caneta: ");
+        cor = entrada.next();
+
+        System.out.println("Entre com o tamanho da caneta: ");
+        tamanho = entrada.nextDouble();
+
+        Caneta novaCaneta = new Caneta(marca, cor, tamanho);
+        System.out.println(novaCaneta.toString());
+
+        String vetor[] = new String[5];
+        int vetorInt[] = new int[5];
+
+        Caneta[] colecaoDeCanetas = new Caneta[10];
+        colecaoDeCanetas[0] = new Caneta("montblanc", "preta", 0.2);
+        colecaoDeCanetas[1] = new Caneta("Hugo Boss", "Dourada", 0.1);
+        colecaoDeCanetas[2] = new Caneta("Stabillo", "Rosa", 0.5);
+
+        System.out.println(colecaoDeCanetas[0].getCor());
+        System.out.println(colecaoDeCanetas[0].toString());
+        
+        for (int i = 0; i < colecaoDeCanetas.length; i++) {
+            System.out.println(colecaoDeCanetas[i].toString());
+        }
         
     }
 }
