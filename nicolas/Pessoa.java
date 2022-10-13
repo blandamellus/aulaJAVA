b0beedd5fc447bfc28122999bb5c2c397fc06aa0
Nicolaspiovovar <112664659+Nicolaspiovovar@users.noreply.github.com>
2022-10-13 18:57:33 -0300

    Add files via upload

---
+++ nicolas/Pessoa.java
@@ -0,0 +1,40 @@
+public class Pessoa{
+   private String nome;
+   private String altura;
+   private String idade;
+    Double peso;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        if (!nome.contains("teste")){
+            this.nome = nome;
+            System.out.println(this.nome);
+        } else
+            System.out.println("nome invalido");
+        }
+
+    public void andar(){
+        System.out.println("andando");
+
+    }
+    public void comer(){
+        System.out.println("comendo");
+    }
+}
+
+class Aluno extends Pessoa {
+    String curso;
+    String disciplina;
+    Boolean preguiça;
+
+    public void estudar(){
+        System.out.println("estudando");
+    }
+    public void comer(){
+        System.out.println("comendo comida vegana...");
+    }
+
+}
