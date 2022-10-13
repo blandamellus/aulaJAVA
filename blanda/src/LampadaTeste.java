4d3e403ec7c5c9654a33c05cdeb6d85a3645b3d3
blandamellus <blandamellus@gmail.com>
2022-10-13 20:29:19 -0300

    teasasd

---
+++ blanda/src/LampadaTeste.java
@@ -0,0 +1,33 @@
+import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+
+public class LampadaTeste {
+    public static void main(String[] args) throws Exception {
+
+        Lampada led = new Lampada();
+        led.setTipo("led");
+        led.setVoltagem(220);
+        led.setCor("verde");
+        led.setMarca("Elgin Supreme");
+        led.setPreco(11.15);
+        led.setPotencia(100);
+        led.setStatus(false);
+        led.toString();
+        System.out.println(led.toString());
+
+        Lampada florescente = new Lampada("florescente", 110, "branco", "phillips", 30.0, 350, true);
+           System.out.println(florescente.toString());
+
+
+        Lampada incandescente = new Lampada();
+        incandescente.setTipo("Incandescente");
+        incandescente.setVoltagem(220);
+        incandescente.setCor("Amarela");
+        incandescente.setMarca("Taschibra");
+        incandescente.setPreco(25.0);
+        incandescente.setPotencia(300);
+        incandescente.ligar();
+
+        System.out.println(incandescente.toString());
+    }
+
+}
